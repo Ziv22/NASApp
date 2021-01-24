58024628f5579f5022d6b916edf9179675031b50
ziv221 <ziv.itzh@gmail.com>
2021-01-24 10:42:47 +0200

    fixed a bug

--- src/App.js
+++ src/App.js
@@ -1,4 +1,3 @@
-import { usePromiseTracker } from "react-promise-tracker";
 import { BrowserRouter as Router} from 'react-router-dom'
 import Container  from './components/Container'
 import NavBar     from './components/NavBar';
@@ -6,18 +5,10 @@ import React      from 'react';
 import './App.css';
 
 function App() {
-  const LoadingIndicator = props => {
-    const { promiseInProgress } = usePromiseTracker();
-      return (
-        promiseInProgress &&
-       <img src= "../assets/circle.png" alt=""/>
-     );  
-}
   return (
       <Router>
         <div className="App">
             <NavBar/>
-            <LoadingIndicator/>
             <Container/>
         </div>
       </Router>
